7d2ff00d97310f7e02c831a49f0d266689f927a9
Warlight991 <zsoltszendrei6@gmail.com>
2025-02-27 12:50:39 +0100

    egyben az egész

---
+++ Gyakolás2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakolás2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ora, indulas = 0;
+            string visszaszamlalas;
+            Console.Write("Hány óras visszaszámlálást tervezünk? ");
+            ora = int.Parse(Console.ReadLine());
+            indulas = ora;
+
+
+
+            do
+            {
+                Console.WriteLine("Visszamlálás: " + ora);
+                Console.Write("Fel kell függeszteni a visszaszamlálást (i/n)? ");
+                visszaszamlalas = Console.ReadLine();
+                if (visszaszamlalas == "n")
+                {
+                    ora--;
+                }
+                else
+                {
+                    indulas++;
+                    ora--;
+                }
+            }
+            while (ora > 0);
+
+
+
+            
+        
+
+
+            Console.WriteLine($"A rakéta a visszaszámlálást követően ennyi óraval indult: {indulas}");
+            
+            
+            
+            
+            Console.ReadKey();
+
+        }
+    }
+}
